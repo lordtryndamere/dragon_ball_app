e0101ab8d4c162e12b379cba33d1abf5a2c54a96
FlutterAwesome <flutterawesome@iMac-Pro-de-FlutterAwesome.local>
2022-02-18 21:33:14 -0500

    finish all app movies

--- lib/screens/details_film_screen.dart
+++ lib/screens/details_film_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
-import 'package:dragon_ball_app/themes/app_theme.dart';
+import 'package:animate_do/animate_do.dart';
+import 'package:dragon_ball_app/providers/movies_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../models/models.dart';
 
 class DetailsFilmsScreen extends StatelessWidget {
@@ -14,9 +16,10 @@ class DetailsFilmsScreen extends StatelessWidget {
       body: SafeArea(
         top: true,
         child: Container(
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
               image: DecorationImage(
-                  image: AssetImage('assets/backgroundetail.jpeg'),
+                  image: NetworkImage(
+                      'https://i.pinimg.com/originals/a4/b3/6a/a4b36a2211d6a6aa49c75b7bcb832f6a.gif'),
                   fit: BoxFit.cover)),
           width: double.infinity,
           height: double.infinity,
@@ -25,14 +28,24 @@ class DetailsFilmsScreen extends StatelessWidget {
               _CustomAppBar(movie: movie),
               SliverList(
                   delegate: SliverChildListDelegate([
-                Column(
-                  children: [
-                    FittedText(movie: movie, size: size),
-                    ClipRRect(child: _OverView(movie: movie)),
-                    const SizedBox(
-                      height: 32,
-                    )
-                  ],
+                BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 4.5, sigmaY: 4.5),
+                  child: Column(
+                    children: [
+                      FittedText(
+                        movie: movie,
+                        size: size,
+                      ),
+                      _OverView(movie: movie),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      _MovieSlider(category: movie.categoryId),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                    ],
+                  ),
                 )
               ]))
             ],
@@ -56,25 +69,11 @@ class _OverView extends StatelessWidget {
     final size = MediaQuery.of(context).size;
     return Container(
       padding: const EdgeInsets.only(left: 20, top: 32, right: 20),
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Image(
-            height: size.height * 0.2 + 40,
-            image: const AssetImage('assets/iconZ.png'),
-          ),
-          BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 4, sigmaY: 4),
-            child: Text(
-              movie.sinopsis,
-              textAlign: TextAlign.justify,
-              style: const TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16),
-            ),
-          ),
-        ],
+      child: Text(
+        movie.sinopsis,
+        textAlign: TextAlign.justify,
+        style: const TextStyle(
+            color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16),
       ),
     );
   }
@@ -91,6 +90,10 @@ class _CustomAppBar extends StatelessWidget {
     final size = MediaQuery.of(context).size;
 
     return SliverAppBar(
+      leading: IconButton(
+        icon: const Icon(Icons.arrow_back_ios),
+        onPressed: () => Navigator.pushNamed(context, 'movies'),
+      ),
       expandedHeight: size.height * 0.3,
       pinned: false,
       backgroundColor: Colors.orange[100],
@@ -159,6 +162,105 @@ class FittedText extends StatelessWidget {
             fontWeight: FontWeight.w800,
             color: Colors.white),
         textAlign: TextAlign.center, // centrar texto en contenido
+      ),
+    );
+  }
+}
+
+class _MovieSlider extends StatelessWidget {
+  const _MovieSlider({
+    Key? key,
+    required this.category,
+  }) : super(key: key);
+  final String category;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final moviesProvider = Provider.of<MoviesProvider>(context);
+    // moviesProvider.getMoviesByCategory(category.id);
+    return Container(
+      // siempre a mi container main ponerle width infinito
+      margin: const EdgeInsets.symmetric(horizontal: 20),
+      width: double.infinity,
+      height: size.height * 0.3 - 18,
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Row(
+          children: [
+            Image(
+                image: const AssetImage('assets/icon_ball.png'),
+                height: size.height * 0.1 - 62),
+            const SizedBox(width: 15),
+            Text('Similares en esta categoria..',
+                style: TextStyle(
+                    //backgroundColor: Colors.white,
+                    //  background: Paint()..color = Colors.orange[900]!,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w900,
+                    fontSize: size.height * 0.1 - 63)),
+          ],
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        FutureBuilder(
+          builder: (_, AsyncSnapshot<List<Movie>> snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator.adaptive(),
+              );
+            }
+            final movies = snapshot.data!;
+            return SizedBox(
+              width: double.infinity,
+              height: size.height * 0.2 + 25,
+              child: movies.isNotEmpty
+                  ? ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) => FadeIn(
+                          delay: const Duration(milliseconds: 150),
+                          child: _CustomMoviePoster(movie: movies[index])),
+                      itemCount: movies.length)
+                  : const Center(
+                      child: CircularProgressIndicator.adaptive(),
+                    ),
+            );
+          },
+          future: moviesProvider.findByCategory(category),
+        ),
+      ]),
+    );
+  }
+}
+
+class _CustomMoviePoster extends StatelessWidget {
+  const _CustomMoviePoster({
+    Key? key,
+    required this.movie,
+  }) : super(key: key);
+  final Movie movie;
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final date = DateTime.now().millisecond.ceilToDouble();
+    movie.heroId = 'movie-slider-detail-$date-${movie.name}';
+    return GestureDetector(
+      onTap: () =>
+          Navigator.pushNamed(context, 'movies-detail', arguments: movie),
+      child: Container(
+        margin: const EdgeInsets.only(right: 10, bottom: 10),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Hero(
+            tag: movie.heroId!,
+            child: FadeInImage(
+              image: NetworkImage(movie.posterImg),
+              placeholder: const AssetImage('assets/loading.gif'),
+              width: size.width * 0.3 - 20,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
       ),
     );
   }
